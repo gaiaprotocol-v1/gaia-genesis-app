5c63528b0a8d3dc8c58d5baab0f5dff7b871471e
dilrong <dilrong@dilrong.com>
2022-01-30 19:02:50 +0900

    feat: 모바일 대응

--- src/view/Dashboard.ts
+++ src/view/Dashboard.ts
@@ -10,6 +10,28 @@ export default class Mining implements View {
     constructor() {
         Layout.current.content.append(
             this.container = el(".dashboard-view", { "data-aos": "zoom-in" },
+                el(".title",
+                    el("h1", "Gaia Protocol"),
+                    el("p", "Summary Your NFT")
+                ),
+                el(".content",
+                    el(".content-wrap",
+                        el("h2", "KRNO Price"),
+                        el("p", "$316"),
+                    ),
+                    el(".content-wrap",
+                        el("h2", "APY"),
+                        el("p", "150,760.4%"),
+                    ),
+                    el(".content-wrap",
+                        el("h2", "Buyback Fund"),
+                        el("p", "$231,321"),
+                    ),
+                    el(".content-wrap",
+                        el("h2", "Your sKRNO Balance"),
+                        el("p", "0"),
+                    )
+                )
             ).appendTo(BodyNode)
         )
     }
