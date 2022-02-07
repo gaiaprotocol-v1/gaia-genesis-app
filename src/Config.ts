5eed40864c7b12753e73aaaa4779619825a8389a
Hanul <hanul@hanul.me>
2022-02-07 15:28:43 +0900

    계약 추가

---
+++ src/Config.ts
@@ -0,0 +1,8 @@
+export default {
+    chainId: 8217,
+    contracts: {
+        GaiaNFT: "0xed34a0296d347cc05972d45ff7e2bbc672544cf3",
+        GaiaOperation: "0xEDB54d8274AFa77E4f56b8C74861425ee1D1f281",
+        GaiaBuyBackFund: "0x208a101103d1a2Da529532CCe0DFe98B3723Bc20",
+    },
+};
